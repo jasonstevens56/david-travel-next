429db14806162665fdb7dce3a94fdce4bdb49e20
jasonstevens56 <jasonstevens56@users.noreply.github.com>
2026-05-05 17:02:57 -0400

    Fix Sanity schema build error

--- sanity/env.ts
+++
@@ -1,3 +0,0 @@
-export const apiVersion = '2026-05-04'
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'e4d8eg4j'
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -31,7 +31,11 @@ const post = defineType({
     defineField({name: 'slug', title: 'Slug', type: 'slug', options: {source: 'title', maxLength: 96}, validation: (Rule) => Rule.required()}),
     defineField({name: 'author', title: 'Author', type: 'reference', to: [{type: 'author'}]}),
     defineField({name: 'mainImage', title: 'Main image', type: 'image', options: {hotspot: true}}),
-    defineField({name: 'categories', title: 'Categories', type: 'array', of: [{type: 'reference', to: [{type: 'category'}]}),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'category'}]}],
     }),
     defineField({name: 'publishedAt', title: 'Published at', type: 'datetime'}),
     defineField({name: 'excerpt', title: 'Excerpt', type: 'text'}),
